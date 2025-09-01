f9dc5a93282f0365ccc4f24817fead2a7c338065
Denisa Marcisovska <dmarcisovska@gmail.com>
2025-08-31 17:49:35 -0700

    formatting and added date converter function

--- src/components/Api.jsx
+++ src/components/Api.jsx
@@ -1,31 +1,36 @@
 import { useState } from "react";
-import {
-  QueryClient,
-  useQuery,
-} from "@tanstack/react-query";
-
-const queryClient = new QueryClient();
+import { useQuery } from "@tanstack/react-query";
 
 export default function Api() {
   const { isPending, error, data } = useQuery({
     queryKey: ["repoData"],
     queryFn: () =>
-      fetch("https://openholidaysapi.org/PublicHolidays?countryIsoCode=NL&languageIsoCode=EN&validFrom=2025-01-01&validTo=2025-12-31").then((res) =>
-        res.json()
-      ),
+      fetch(
+        "https://openholidaysapi.org/PublicHolidays?countryIsoCode=NL&languageIsoCode=EN&validFrom=2025-01-01&validTo=2025-12-31"
+      ).then((res) => res.json()),
   });
 
   if (isPending) return "Loading...";
 
   if (error) return "An error has occurred: " + error.message;
-   console.log(data)
+  console.log(data);
+
+  function convert_date(iso) {
+    let my_date = new Date(iso);
+    let formatted_date = new Intl.DateTimeFormat("en", {
+      month: "short",
+      day: "2-digit",
+    }).format(my_date);
+
+    return formatted_date;
+  }
 
   return (
     <div>
-     <ul>
-        {data?.map(holiday => (
+      <ul>
+        {data?.map((holiday) => (
           <li key={holiday.id}>
-            {holiday.name[0].text} - {holiday.startDate}
+            - {convert_date(holiday.startDate)} {holiday.name[0].text}
           </li>
         ))}
       </ul>
